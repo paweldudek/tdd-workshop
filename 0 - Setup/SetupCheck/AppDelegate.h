a5d1bf62d057cb6f0d3f128abe9cc349832b4152
Pawel Dudek <pawel@dudek.mobi>
2014-03-12 10:47:01 +0100

    Moved stuff around

---
+++ 0 - Setup/SetupCheck/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SetupCheck
+//
+//  Created by Pawel Dudek on 12/03/14.
+//  Copyright (c) 2014 MobileWarsaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 0 - Setup/SetupCheckSpecs/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SetupCheckSpecs
+//
+//  Created by Pawel Dudek on 12/03/14.
+//  Copyright (c) 2014 MobileWarsaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <Cedar-iOS/Cedar-iOS.h>
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"CedarApplicationDelegate");
+    }
+}
