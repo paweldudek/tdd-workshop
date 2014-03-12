5c7bee983b3e5f997e4c0e58fba9162ee1c1739e
Pawel Dudek <pawel@dudek.mobi>
2014-03-12 10:53:19 +0100

    Cleaned up and added test project to check whether everyone has the right setup

--- 0 - Setup/SetupCheckSpecs/ExampleSpec.m
+++ 0 - Setup/SetupCheckSpecs/ExampleSpec.m
@@ -8,26 +8,20 @@ SPEC_BEGIN(ExampleSpec)
 describe(@"Example specs on NSString", ^{
 
     it(@"lowercaseString returns a new string with everything in lower case", ^{
-        [@"FOOBar" lowercaseString] should equal(@"foobar");
+         expect([@"FOOBar" lowercaseString]).to.equal(@"foobar");
     });
 
     it(@"length returns the number of characters in the string", ^{
-        [@"internationalization" length] should equal(20);
+        expect([@"internationalization" length]).to.equal(20);
     });
 
     describe(@"isEqualToString:", ^{
         it(@"should return true if the strings are the same", ^{
-            [@"someString" isEqualToString:@"someString"] should be_truthy;
+            expect([@"someString" isEqualToString:@"someString"]).to.beTruthy();
         });
 
         it(@"should return false if the strings are not the same", ^{
-            [@"someString" isEqualToString:@"anotherString"] should_not be_truthy;
-        });
-    });
-
-    describe(@"NSMutableString", ^{
-        it(@"should be a kind of NSString", ^{
-            [NSMutableString string] should be_instance_of([NSString class]).or_any_subclass();
+            expect([@"someString" isEqualToString:@"anotherString"]).to.beFalsy();
         });
     });
 });
