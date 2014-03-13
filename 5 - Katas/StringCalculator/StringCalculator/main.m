99d15ec470b876c531dbec1e9ca0a00614f26357
Pawel Dudek <pawel@dudek.mobi>
2014-03-13 19:55:04 +0100

    And added third kata

---
+++ 5 - Katas/StringCalculator/StringCalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StringCalculator
+//
+//  Created by Pawel Dudek on 13/03/14.
+//  Copyright (c) 2014 Mobile-Warsaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
