a07bdea6756365e0df0e9951c508efe2c3f5c5a2
Pawel Dudek <pawel@dudek.mobi>
2014-03-15 17:08:06 +0100

    Added basic tests

--- 1 - Introduction/FirstTest/FirstTestSpecs/NSString+FirstWordSentenceCapitalizerSpec.m
+++ 1 - Introduction/FirstTest/FirstTestSpecs/NSString+FirstWordSentenceCapitalizerSpec.m
@@ -1,12 +1,46 @@
 #import "NSString+FirstTwoWordsSentenceCapitalizer.h"
-#import "CDRSpec.h"
+
+#define EXP_SHORTHAND
+#import "Expecta.h"
+
+#define HC_SHORTHAND
+#import <OCHamcrest/OCHamcrest.h>
+
+#define MOCKITO_SHORTHAND
+#import "OCMockito.h"
 
 SPEC_BEGIN(NSStringFirstWordSentenceCapitalizerSpec)
 
 describe(@"NSString+FirstTwoWordsSentenceCapitalizer", ^{
 
+    context(@"when the string has only one word", ^{
+        __block NSString *sentence;
 
+        beforeEach(^{
+            sentence = [@"fixture" stringByCapitalizingFirstTwoWords];
+        });
 
+        it(@"should return string with capitalized first two words", ^{
+            expect(sentence).to.equal(@"Fixture Sentence text");
+        });
+    });
+
+    context(@"when the string has three words", ^{
+
+        __block NSString *sentence;
+
+        beforeEach(^{
+            sentence = [@"fixture sentence text" stringByCapitalizingFirstTwoWords];
+        });
+
+        afterEach(^{
+            sentence = nil;
+        });
+
+        it(@"should return string with capitalized first two words", ^{
+            expect(sentence).to.equal(@"Fixture Sentence text");
+        });
+    });
 });
 
 SPEC_END
